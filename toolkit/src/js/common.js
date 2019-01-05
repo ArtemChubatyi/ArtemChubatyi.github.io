835caa6bac5a571cab5574c4e5e256e71011efab
ArtemChubatyi <stgmdota@gmail.com>
2019-01-05 10:18:33 +0200

    updated toolkit

--- toolkit/src/js/common.js
+++ toolkit/src/js/common.js
@@ -1,57 +1,70 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	// ------Sidebar-------
-	$(".sidebar-menu > li.have-children a").on("click", function(){	
-    if( ! $(this).parent().hasClass("active") ){
-      $(".sidebar-menu li ul").slideUp();
-      $(this).next().slideToggle();
-      $(".sidebar-menu li").removeClass("active");
-      $(this).parent().addClass("active");
-    }
-    else{
-      $(this).next().slideToggle();
-      $(".sidebar-menu li").removeClass("active");
-        }
+	$(".sidebar-menu > li.have-children a").on("click", function () {
+		if (!$(this).parent().hasClass("active")) {
+			$(".sidebar-menu li ul").slideUp();
+			$(this).next().slideToggle();
+			$(".sidebar-menu li").removeClass("active");
+			$(this).parent().addClass("active");
+		}
+		else {
+			$(this).next().slideToggle();
+			$(".sidebar-menu li").removeClass("active");
+		}
 	});
-	
+
 	// --------Speed dial------
-	$(".tk-speed-dial__button").on("click", function(){	
+	$(".tk-speed-dial__button").on("click", function () {
 		$(".tk-speed-dial ul").slideToggle();
-		$(".tk-speed-dial__item").on("click", function(){	
+		$(".tk-speed-dial__item").on("click", function () {
 			$(".tk-speed-dial ul").slideUp();
 		});
 	});
 
 	// --------Menu whith dropdown------
-	$(".tk-menu__item.have-children > .tk-item__content").on("click", function(){
-		if(!$(this).parent().hasClass("active")) {
+	$(".tk-menu__item.have-children > .tk-item__content").on("click", function () {
+		if (!$(this).parent().hasClass("active")) {
 			$(this).parent().addClass("active");
-		} else { 
+		} else {
 			$(this).parent().removeClass("active");
 		}
 		$(this).next().slideToggle();
-		$(".tk-menu__inner-item").on("click", function(){	
+		$(".tk-menu__inner-item").on("click", function () {
 			$(".tk-menu__item ul").slideUp();
 			$(".tk-menu__item").removeClass("active");
 		});
 	});
 
-	// --------Tabs------
-	$(".tk-tabs > .tk-tabs__item").on("click", function(){
-		if(!$(this).hasClass("active")) {
-			$(this)
-				.addClass("active").siblings().removeClass("active")
-		}
-	})
+	// --------modal toast------
+	document.querySelector('.toast-examples > button').onclick = function () {
+		let toast = document.querySelector('.toast-examples > .tk-toast');
+		toast.classList.add('tk-toast_show');
+		setTimeout(function () {
+			toast.classList.remove('tk-toast_show');
+		}, 4000);
+	}
 
-	$(".tk-tabs-title").on("click", function () {
-		$(this).next().slideToggle();
-	})
+	// --------modal dialog------
+	let modal = document.querySelector('[data-modal="dialog"]');
+	let btn = document.querySelector('[data-onclick="dialog"]');
+	let overflow = document.createElement('div');
+	overflow.className = "overflow";
+
+	btn.onclick = function () {
+		document.body.appendChild(overflow);
+		modal.style.display = "flex";
+	}
+
+	overflow.onclick = function () {
+		overflow.remove();
+		modal.style.display = "none";
+	}
 
 	// --------Top bar------
 	var prevScrollPos = window.pageYOffset;
-	window.onscroll = function() {
+	window.onscroll = function () {
 		var curentScrollPos = window.pageYOffset;
-		if(prevScrollPos > curentScrollPos) {
+		if (prevScrollPos > curentScrollPos) {
 			document.getElementById('header').style.top = "0";
 		} else {
 			document.getElementById('header').style.top = "-80px";
